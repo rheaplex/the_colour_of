ecf9a4d144859e072f63cdfd95ee2bb12888f545
Rob Myers <robmyers@robs-laptop.(none)>
2009-08-20 23:00:27 +0100

    Use official pasenger recipe for capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,13 +58,16 @@ end
 ################################################################################
 
 namespace :deploy do
-  desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{current_path}/tmp/restart.txt"
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
   end
 
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles => :app do ; end
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
   end
 end
